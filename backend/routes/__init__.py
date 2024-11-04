389535fbc7d08dcda418dc83d58b021d2282a179
thaag7734 <thaag7734@gmail.com>
2024-11-04 13:19:14 -0500

    remove old starter code

--- backend/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from labels import labels
-from ..app import app
-
-
-
-app.register_blueprint(labels)
--- backend/routes/labels/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-labels: Blueprint = Blueprint("labels", __name__, url_prefix="/labels")
-
-@labels.route("/", methods=["GET"])
-async def get_labels():
-   pass
