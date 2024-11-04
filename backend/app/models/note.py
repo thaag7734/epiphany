a06776408c778ef167c1d1233a53289880c81933
luke <lukecleveland1992@gmail.com>
2024-11-04 15:56:07 -0500

    notes schema finished

---
+++ backend/app/models/note.py
@@ -0,0 +1,11 @@
+from .db import db
+from sqlalchemy import CheckConstraint
+
+class Note(db.Model):
+      __tablename__ = 'notes'
+
+      id = db.Column(db.Integer, primary_key=True)
+      title = db.Column(db.String(32), nullable=False)
+      content = db.Column(db.String(2000))
+      deadline = db.Column(db.Date)
+      priority = db.Column(db.Integer, CheckConstraint('priority >= 0 AND priority <= 3'))
