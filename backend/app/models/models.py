0ed3f0f171fca3c3862892afcf683bf99fad9791
luke <lukecleveland1992@gmail.com>
2024-11-07 12:12:06 -0500

    sign up route and Board model

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import CheckConstraint, ForeignKey, column
+from datetime import datetime
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
@@ -96,11 +97,11 @@ class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     team_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("teams.id")))
     owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False, default=datetime.now().strftime('%d-%m-%Y'))
 
-    team = db.relationship("Team", back_populates="board")
-    labels = db.relationship("Label", back_populates="board")
-    notes = db.relationship("Note", back_populates="board")
+    team = db.relationship("Team", back_populates="board", cascade="all, delete-orphan")
+    labels = db.relationship("Label", back_populates="board", cascade="all, delete-orphan")
+    notes = db.relationship("Note", back_populates="board", cascade="all, delete-orphan")
     owner = db.relationship("User", back_populates="boards", foreign_keys=[owner_id])
 
     def to_dict(self):
