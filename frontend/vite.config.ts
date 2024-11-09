a0a908d4adbb0a5afde6d9e585bf0d3d74d01140
thaag7734 <thaag7734@gmail.com>
2024-11-09 02:58:46 -0500

    alias src dir to "@"

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000"
+    },
+    host: "0.0.0.0"
+  },
+  resolve: {
+    alias: {
+      "@": "./src"
+    }
+  }
 })
