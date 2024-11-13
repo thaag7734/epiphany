383f21927ee4453c8fef6a0dbf23bdfcc03437c4
luke <lukecleveland1992@gmail.com>
2024-11-13 17:43:30 -0500

    fixed top nav merge add migration

--- frontend/src/components/top_nav/TopNav.tsx
+++ frontend/src/components/top_nav/TopNav.tsx
@@ -1,16 +1,7 @@
-<<<<<<< Updated upstream
-// import { IoIosHelpCircle } from "react-icons/io";
-import FileDropdown from "./file_dropdown";
-=======
->>>>>>> Stashed changes
 import "./TopNav.css";
 import UserDropdown from "./user_dropdown/UserDropdown";
 
 export default function TopNav() {
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
   return (
     <nav className="topNav">
       <div className="userButtons">
