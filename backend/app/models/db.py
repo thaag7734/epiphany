08fd608846a115f8c52dbf98e194c110d20445fc
thaag7734 <thaag7734@gmail.com>
2024-11-14 02:26:44 -0500

    call load_dotenv in db.py

--- backend/app/models/db.py
+++ backend/app/models/db.py
@@ -1,11 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy as sa
-
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
 environment = os.environ.get("FLASK_ENV")
 SCHEMA = os.environ.get("SCHEMA")
 
 
 db: sa = sa()
+
 
 # helper function for adding prefix to foreign key column references in production
 def add_prefix_for_prod(attr):
