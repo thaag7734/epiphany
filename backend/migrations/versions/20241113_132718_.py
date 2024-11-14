27bf1bf114e94ecba6023e470eca3e5983bf6c61
thaag7734 <thaag7734@gmail.com>
2024-11-13 23:49:29 -0500

    not even a clue what i'm doing actually

--- backend/migrations/versions/20241113_132718_.py
+++ backend/migrations/versions/20241113_132718_.py
@@ -1,4 +1,4 @@
-"""empty message
+"""create tables
 
 Revision ID: e88bf513bb93
 Revises:
@@ -74,67 +74,6 @@ def upgrade():
         sa.PrimaryKeyConstraint("note_id", "label_id"),
     )
     # ### end Alembic commands ###
-
-    op.create_foreign_key(
-        "fk_boards_owner_id", "boards", "users", ["owner_id"], ["id"], schema="epiphany"
-    )
-    op.create_foreign_key(
-        "fk_boards_team_id", "boards", "teams", ["team_id"], ["id"], schema="epiphany"
-    )
-    op.create_foreign_key(
-        "fk_teams_owner_id", "teams", "users", ["owner_id"], ["id"], schema="epiphany"
-    )
-    op.create_foreign_key(
-        "fk_users_root_board_id",
-        "users",
-        "boards",
-        ["root_board_id"],
-        ["id"],
-        schema="epiphany",
-    )
-    op.create_foreign_key(
-        "fk_labels_board_id",
-        "labels",
-        "boards",
-        ["board_id"],
-        ["id"],
-        schema="epiphany",
-    )
-    op.create_foreign_key(
-        "fk_notes_board_id", "notes", "boards", ["board_id"], ["id"], schema="epiphany"
-    )
-    op.create_foreign_key(
-        "fk_team_users_team_id",
-        "team_users",
-        "teams",
-        ["team_id"],
-        ["id"],
-        schema="epiphany",
-    )
-    op.create_foreign_key(
-        "fk_team_users_user_id",
-        "team_users",
-        "users",
-        ["user_id"],
-        ["id"],
-        schema="epiphany",
-    )
-    op.create_foreign_key(
-        "fk_note_labels_note_id",
-        "note_labels",
-        "notes",
-        ["note_id"],
-        ["id"],
-        schema="epiphany",
-    )
-    op.create_foreign_key(
-        "fk_note_labels_label_id",
-        "note_labels",
-        "notes",
-        ["label_id"],
-        ["id"],
-        schema="epiphany",
-    )
 
 
 def downgrade():
