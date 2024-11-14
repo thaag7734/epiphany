3acc686a7e91ca271604fb3b075000e256c30acd
thaag7734 <thaag7734@gmail.com>
2024-11-13 23:07:32 -0500

    schema (i'm going crazy)

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -5,8 +5,18 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 team_user = db.Table(
     "team_users",
-    db.Column("team_id", db.Integer, db.ForeignKey("teams.id"), primary_key=True),
-    db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column(
+        "team_id",
+        db.Integer,
+        db.ForeignKey(add_prefix_for_prod("teams.id")),
+        primary_key=True,
+    ),
+    db.Column(
+        "user_id",
+        db.Integer,
+        db.ForeignKey(add_prefix_for_prod("users.id")),
+        primary_key=True,
+    ),
     schema=SCHEMA if environment == "production" else None,
 )
 
