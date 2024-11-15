e2cfc3103c757a13c8590536fbc652ab323738de
zev-b <zevbistritz@gmail.com>
2024-11-15 00:14:51 -0500

    adds fixes post-merge, to stylings of modal

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -59,7 +59,9 @@ export function Modal() {
 
     // Render the following component to the div referenced by the modalRef
     return ReactDOM.createPortal(
+        <div id="modal">
         <div id="modal-background" onMouseDown={closeModal}>
+        </div>
             <div className="modal-content">{modalContent}</div>
         </div>,
         modalRef.current
