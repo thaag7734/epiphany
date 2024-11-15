509b778d175e1d321fde87ea5e5133709f9fa17f
andrew <a.myrold@att.net>
2024-11-15 12:24:05 -0500

    gangster

--- frontend/src/components/top_nav/TopNav.tsx
+++ frontend/src/components/top_nav/TopNav.tsx
@@ -1,20 +1,25 @@
+import { useParams } from "react-router";
 import "./TopNav.css";
 import UserDropdown from "./user_dropdown/UserDropdown";
 import { SlArrowRight } from "react-icons/sl";
 
 export default function TopNav() {
-   function showSidePanel(): void {
-      const sidePanel = document.querySelector("#side-panel");
-      if (!sidePanel) return;
-      sidePanel.id = sidePanel.id.concat("-show");
-   }
+    const { boardId } = useParams();
+    function showSidePanel(): void {
+        const sidePanel = document.querySelector("#side-panel");
+        if (!sidePanel) return;
+        sidePanel.id = sidePanel.id.concat("-show");
+    }
 
-   return (
-      <nav className="top-nav">
-         <SlArrowRight onMouseEnter={showSidePanel} className="arrow-box" />
-         <div className="user-buttons">
-            <UserDropdown />
-         </div>
-      </nav>
-   );
+    return (
+        <nav className="top-nav">
+            <SlArrowRight
+                onMouseEnter={showSidePanel}
+                className={`arrow-box ${boardId ? "" : "invisible"}`}
+            />
+            <div className="user-buttons">
+                <UserDropdown />
+            </div>
+        </nav>
+    );
 }
